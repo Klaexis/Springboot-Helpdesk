a2d5f8c23680d1bcbb35f648ec125dd12f96a670
Aleck <alecklim1245@gmail.com>
2025-12-05 15:35:56 +0800

    Added DTO

---
+++ dto/src/main/java/com/helpdesk/dto/TicketUpdateDTO.java
@@ -0,0 +1,20 @@
+package com.helpdesk.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.helpdesk.model.TicketStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Optional;
+
+@Setter
+@Getter
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class TicketUpdateDTO {
+    private Optional<String> ticketTitle = Optional.empty();
+    private Optional<String> ticketBody = Optional.empty();
+    private Optional<Long> ticketAssigneeId = Optional.empty();
+    private Optional<TicketStatus> ticketStatus = Optional.empty();
+    private Optional<String> ticketRemarks = Optional.empty();
+
+}
